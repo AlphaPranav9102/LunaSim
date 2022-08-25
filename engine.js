d9545b98a17f121a2d1f80cb7e293eea693fa85d
oboy-1 <orangegeoboy@gmail.com>
2022-08-25 14:51:28 -0400

    New RK4, Timestep
    * Fixed rk4() bug by having it find k_n values for all stocks and then updating each stock's safevalue.
    
    * Fixed time issue (where simulation would go one timestep more or less).  It was due to floating point error.  TIME TRACKING WORKS UP TO 5 DECIMAL PLACES.
    
    * Formatting changes

--- engine.js
+++ engine.js
@@ -6,14 +6,14 @@ class Simulation {
         this.startTime = parseFloat(this.data.start_time);
         this.endTime = parseFloat(this.data.end_time);
 
-        this.initStocks(); // set initial values for stocks
+        this.initObjects(); // set initial values for stocks
     }
 
     /* 
-    Replaces names in flowEq with values.
-    Example: 'converter1*converter2+stock1' --> '1*2+3'
+    Replaces names in equation with values.
+    Example: 'converter1*converter2+stock1' --> '(1)*(2)+(3)'
     */
-    parseFlow(flowEq) {
+    parseObject(equation) {
         let objects = {} // stores all stocks, converters, and flows and their respective equation/safeval
 
         for (var stock in this.data.stocks) {
@@ -35,27 +35,43 @@ class Simulation {
 
         let sortedObjects = Object.keys(objects).sort((a, b) => a.length - b.length).reverse() // sort by length (descending) to prevent substring errors
 
-        // Call parseFlow recursively on all objects to replace the names with their respective values
+        // Call parseObject recursively on all objects to replace the names with their respective values
         for (var object of sortedObjects) {
-            if (flowEq.includes(object)) {
-                flowEq = flowEq.replace(object, '(' + objects[object] + ')'); // RECURSIVE
+            if (equation.includes(object)) {
+                equation = equation.replace(object, this.parseObject('(' + objects[object] + ')')); // RECURSIVE
             }
         }
 
-        return flowEq;
+        return equation;
     }
 
     /*
-    Applies parseFlow initially to the stock values to work out the "static converters"
+    Applies parseObject initially all values to figure out timestep 0.
     */
-    initStocks() {
+    initObjects() {
         for (var stockName in this.data.stocks) {
             let stock = this.data.stocks[stockName];
 
-            let value = eval(this.parseFlow(stock["equation"]));
+            let value = eval(this.parseObject(stock["equation"]));
             
             stock["safeval"] = value;
             stock["values"].push(value);
+        }
+
+        for (var stockName in this.data.stocks) {
+            let stock = this.data.stocks[stockName];
+
+            // initialize flows
+            for (var flowName in stock["inflows"]) {
+                this.data.stocks[stockName]["inflows"][flowName]["values"].push(eval(this.parseObject(this.data.stocks[stockName]["inflows"][flowName]["equation"])));
+            }
+            for (var flowName in stock["outflows"]) {
+                this.data.stocks[stockName]["outflows"][flowName]["values"].push(eval(this.parseObject(this.data.stocks[stockName]["outflows"][flowName]["equation"])));
+            }
+        }
+
+        for (var converterName in this.data.converters) {
+            this.data.converters[converterName]["values"].push(eval(this.parseObject(this.data.converters[converterName]["equation"])));
         }
     }
 
@@ -73,9 +89,9 @@ class Simulation {
             let flowEq = inflows[i]["equation"];
             if (flowEq.includes("$")) { // check if flow is a uniflow
                 flowEq = flowEq.replace('$', '');
-                sumInflow += Math.max(0, eval(this.parseFlow(flowEq)));
+                sumInflow += Math.max(0, eval(this.parseObject(flowEq)));
             } else {
-                sumInflow += eval(this.parseFlow(flowEq));
+                sumInflow += eval(this.parseObject(flowEq));
             }
         }
 
@@ -84,9 +100,9 @@ class Simulation {
             let flowEq = outflows[i]["equation"];
             if (flowEq.includes("$")) { // check if flow is a uniflow
                 flowEq = flowEq.replace('$', '');
-                sumOutflow += Math.max(0, eval(this.parseFlow(flowEq)));
+                sumOutflow += Math.max(0, eval(this.parseObject(flowEq)));
             } else {
-                sumOutflow += eval(this.parseFlow(flowEq));
+                sumOutflow += eval(this.parseObject(flowEq));
             }
         }
 
@@ -97,8 +113,8 @@ class Simulation {
     Runs model using Euler's method.
     */
     euler() {
-        for (var t = this.startTime + this.dt; t <= this.endTime; t += this.dt) { // iterate over time (skip start time as that was covered in this.initStocks)
-            // Calculate new values for all stocks and their flows
+        for (var t = this.startTime + this.dt; parseFloat(t.toFixed(5)) <= parseFloat(this.endTime.toFixed(5)); t += this.dt) { // (skip start time as that was covered in this.initObjects())
+            // Calculate new values for all stocks
             for (var stockName in this.data.stocks) {
                 let stock = this.data.stocks[stockName];
 
@@ -107,8 +123,28 @@ class Simulation {
                 } else {
                     stock["values"].push(stock["safeval"] + this.dydt(stock) * this.dt);
                 }
+            }
 
-                stock["safeval"] = stock["values"][stock["values"].length - 1]; // update safeval
+            // Update safeval for next iteration
+            for (var stockName in this.data.stocks) { 
+                let stock = this.data.stocks[stockName];
+                stock["safeval"] = stock["values"][stock["values"].length - 1];
+            }
+
+            // Update values for all flows
+            for (var stockName in this.data.stocks) {
+                for (var inflow in this.data.stocks[stockName]["inflows"]) {
+                    this.data.stocks[stockName]["inflows"][inflow]["values"].push(eval(this.parseObject(this.data.stocks[stockName]["inflows"][inflow]["equation"])));
+                }
+                for (var outflow in this.data.stocks[stockName]["outflows"]) {
+                    this.data.stocks[stockName]["outflows"][outflow]["values"].push(eval(this.parseObject(this.data.stocks[stockName]["outflows"][outflow]["equation"])));
+                }
+            }
+
+            // Update the values of all converters
+            for (var converter in this.data.converters) {
+                let converterEq = this.data.converters[converter]["equation"];
+                this.data.converters[converter]["values"].push(eval(this.parseObject(converterEq)));
             }
 
         }
@@ -118,32 +154,90 @@ class Simulation {
     Runs model using 4th order Runge-Kutta method.
     */
     rk4() {
-        for (var t = 0; t <= this.data.endTime; t += this.data.dt) { // iterate over time
-            this.cache["time"].push(t); // add new time to cache
-            for (var stockName in this.data.stocks) { // iterate over stocks
+        for (var t = this.startTime + this.dt; parseFloat(t.toFixed(5)) <= parseFloat(this.endTime.toFixed(5)); t += this.dt) { // use high precision to make sure correct number of iterations
+            let y0_dict = {};
+            let k1_dict = {};
+            let k2_dict = {};
+            let k3_dict = {};
+            let k4_dict = {};
+
+            // Set y0 for every stock
+            for (var stockName in this.data.stocks) {
                 let stock = this.data.stocks[stockName];
-                let y_0 = parseFloat(stock["value"]); // current value
+                y0_dict[stockName] = stock["safeval"];
+            }
+
+            // k1
+            for (var stockName in this.data.stocks) { // calculate k1-values for all stocks (perform for all stocks before updating safeval)
+                let stock = this.data.stocks[stockName];
 
                 // Calculate constants
                 let k1 = this.dydt(stock) * this.dt;
-                stock["value"] = (y_0 + k1 / 2).toString(); // add to the stock for the next k's
+                k1_dict[stockName] = k1;
+            }
+            for (var stockName in this.data.stocks) { // update safevals with new k1 values
+                let stock = this.data.stocks[stockName];
+                stock["safeval"] = y0_dict[stockName] + k1_dict[stockName] / 2;
+            }
+
+            // k2
+            for (var stockName in this.data.stocks) {
+                let stock = this.data.stocks[stockName];
 
                 let k2 = this.dydt(stock) * this.dt;
-                stock["value"] = (y_0 + k2 / 2).toString();
+                k2_dict[stockName] = k2;
+            }
+            for (var stockName in this.data.stocks) {
+                let stock = this.data.stocks[stockName];
+                stock["safeval"] = y0_dict[stockName] + k2_dict[stockName] / 2;
+            }
+
+            // k3
+            for (var stockName in this.data.stocks) {
+                let stock = this.data.stocks[stockName];
 
                 let k3 = this.dydt(stock) * this.dt;
-                stock["value"] = (y_0 + k3).toString();
+                k3_dict[stockName] = k3;
+            }
+            for (var stockName in this.data.stocks) {
+                let stock = this.data.stocks[stockName];
+                stock["safeval"] = y0_dict[stockName] + k3_dict[stockName];
+            }
+
+            // k4 and final value
+            for (var stockName in this.data.stocks) {
+                let stock = this.data.stocks[stockName];
 
                 let k4 = this.dydt(stock) * this.dt;
+                k4_dict[stockName] = k4;
+            }
+            for (var stockName in this.data.stocks) {
+                let stock = this.data.stocks[stockName];
 
-                // Update stock value to final result
-                if (stock["isNN"] == "true") { // check if stock is non-negative
-                    stock["value"] = Math.max(0,(y_0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6)).toString();
-                } else {
-                    stock["value"] = (y_0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6).toString(); 
+                stock["values"].push(y0_dict[stockName] + (k1_dict[stockName] + 2 * k2_dict[stockName] + 2 * k3_dict[stockName] + k4_dict[stockName]) / 6); 
+            }
+            
+
+            // Update safeval for next iteration
+            for (var stockName in this.data.stocks) { 
+                let stock = this.data.stocks[stockName];
+                stock["safeval"] = stock["values"][stock["values"].length - 1];
+            }
+
+            // Update values for all flows
+            for (var stockName in this.data.stocks) {
+                for (var inflow in this.data.stocks[stockName]["inflows"]) {
+                    this.data.stocks[stockName]["inflows"][inflow]["values"].push(eval(this.parseObject(this.data.stocks[stockName]["inflows"][inflow]["equation"])));
                 }
+                for (var outflow in this.data.stocks[stockName]["outflows"]) {
+                    this.data.stocks[stockName]["outflows"][outflow]["values"].push(eval(this.parseObject(this.data.stocks[stockName]["outflows"][outflow]["equation"])));
+                }
+            }
 
-                this.cache[stockName].push(parseFloat(stock["value"])); // add new value to cache
+            // Update the values of all converters
+            for (var converter in this.data.converters) {
+                let converterEq = this.data.converters[converter]["equation"];
+                this.data.converters[converter]["values"].push(eval(this.parseObject(converterEq)));
             }
         }
     }
